0beb5bfcb6132fd343b626beb3a4e07792c67eb6
eulu <anegamega@gmail.com>
2025-03-20 21:27:02 +0200

    Add swagger documentation annotations for CharacterController

--- src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -1,5 +1,7 @@
 package mate.academy.rickandmorty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.dto.internal.CharacterDto;
@@ -12,15 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/characters")
+@Tag(name = "Characters", description = "Operations related to retrieving characters")
 public class CharacterController {
     private final CharacterService characterService;
 
     @GetMapping
+    @Operation(
+            summary = "Get all characters",
+            description = "Get the list of all characters"
+    )
     public List<CharacterDto> getAll() {
         return characterService.getAll();
     }
 
     @GetMapping(params = "name")
+    @Operation(
+            summary = "Search characters by name",
+            description = "Search characters using filter by name ignoring case"
+    )
     public List<CharacterDto> getByName(@RequestParam String name) {
         return characterService.getByName(name);
     }
